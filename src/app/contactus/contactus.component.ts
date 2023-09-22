51d1f49ec459bfa7d4cdf36271af3ff542ab46ab
SubhajitD <subhajitdebnath.999@gmail.com>
2023-09-22 21:31:07 +0530

    obs done

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { DataService } from '../core/services/data.service';
-import { Subscription, interval } from 'rxjs';
+import { Observable, Subscription, filter, interval, map } from 'rxjs';
 
 @Component({
   selector: 'app-contactus',
@@ -13,15 +13,50 @@ export class ContactusComponent {
 
   private firstSub!: Subscription;
 
+  private customObs!: Observable<any>;
+  private customSub!: Subscription;
+
   constructor(
     private dataService: DataService
   ) {}
 
   ngOnInit() {
 
-    this.firstSub = interval(2000).subscribe(count => {
-      console.log(count);
+    this.customObs = Observable.create((observable: any) => {
+      let counter = 1;
+      setInterval(() => {
+        observable.next(counter);
+        counter ++;
+
+        if(counter > 5) {
+          observable.complete();
+        }
+        if(counter === 5) {
+          observable.error(new Error('error happened'))
+        }
+      }, 1000);
     });
+
+    this.customSub = this.customObs
+    .pipe(
+      filter((data) => {
+        return data%2 !== 0;
+      }),
+      map((data) => {
+        return data * 2;
+      })
+    )
+    .subscribe(data => {
+      console.log(data);
+    }, err => {
+      console.log(err.message);
+    });
+
+
+
+    // this.firstSub = interval(2000).subscribe(count => {
+    //   console.log(count);
+    // });
 
 
   }
@@ -29,7 +64,8 @@ export class ContactusComponent {
   ngOnDestroy() {
     console.log('destroy contact us');
 
-    this.firstSub.unsubscribe();
+    this.firstSub?.unsubscribe();
+    this.customSub?.unsubscribe();
   }
 
 }
