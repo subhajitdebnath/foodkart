25fa429d1c3344b2ded9b0a8e78f5fb028fb2986
SubhajitD <subhajitdebnath.999@gmail.com>
2023-11-22 22:18:02 +0530

    find duplicate elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,15 +22,38 @@ export class AppComponent {
       res.hourly.time.forEach((item: any, index: number) => {
         this.mapping[item] = res.hourly.temperature_2m[index];
       });
-      console.log('mapping', this.mapping)
+      // console.log('mapping', this.mapping)
 
       this.filteredItmes = res.hourly.time.filter((date: string) => this.filtercondition.includes(date.slice(-5)));
 
-      console.log('filteredItmes', this.filteredItmes);
+      // console.log('filteredItmes', this.filteredItmes);
 
     }, (err: any) => {
       console.log(err);
     });
+
+
+    let arr = [1, 2, 5, 7, 2, 6, 7, 2];
+    let result = [];
+    for(let i = 0; i < arr.length; i++) {
+      if(result[arr[i]]) {
+        result[arr[i]] += 1;
+      } else {
+        result[arr[i]] = 1;
+      }
+    }
+
+    console.log(result);
+
+
+
+
+
+
+
+
+
+
   }
 
 }
