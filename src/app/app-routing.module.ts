b1c0e7eb6080e63daf40775173262af801a97398
SubhajitD <subhajitdebnath.999@gmail.com>
2023-12-03 18:31:38 +0530

    unit testing intro

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,27 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { FoodListComponent } from './food-list/food-list.component';
-import { ContactusComponent } from './contactus/contactus.component';
-import { TodoComponent } from './todo/todo.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: FoodListComponent
-  },
-  {
-    path: 'contact',
-    component: ContactusComponent
-  },
-  {
-    path: 'todo',
-    component: TodoComponent
-  },
-  {
-    path: 'product/:id',
-    component: ProductDetailComponent
-  }
+  
 ];
 
 @NgModule({
